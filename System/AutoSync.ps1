7b9916980ed6be834ec66d6d74416305b512183d
Wenbin Yang <young24@mail.ustc.edu.cn>
2018-07-19 23:25:07 +0800

    Update AutoSync.ps1

--- System/AutoSync.ps1
+++ System/AutoSync.ps1
@@ -1,32 +1,35 @@
+# ask for permission
+Set-ExecutionPolicy RemoteSigned
 # run the bat of freeFileSync to update the local folder to the remote folder
-Start-Process C:\Users\USER\Documents\Template-Project-Repository\System\syncLocalFolder2Server.ffs_batch
+Start-Process G:\Worm-Thermotaxis\System\syncLocalFolder2Server.ffs_batch
 # Sleep for 2 seconds
 Write-Host "Files Sync Done. Please wait 10 seconds for cleaning temporary caches."
 sleep 10 # seconds
 
 ### In the local folder, automatically create links to link data in the remote folder
-$LocalPath = "C:\Users\USER\Documents\Template-Project-Repository\Data\links"
-$RemotePath = "\\222.195.69.205\wenlab\Young\test\Data"
+$LocalPath = "G:\Worm-Thermotaxis\Data\links"
+$RemotePath = "\\222.195.69.205\wenlab\Projects\B_Taxis_Behaviors_B\Data\thermotaxis"
 
 # if RemotePath or LocalPath does not exist create one
 if (!(Test-Path -path $RemotePath)) {New-Item $RemotePath -ItemType Directory}
-if (!(Test-Path -path $LocalPath)) {New-Item $LocalPath -ItemType Directory)
+if (!(Test-Path -path $LocalPath)) {New-Item $LocalPath -ItemType Directory}
 
 
 # remove invalid links in the local folder
 $links = Get-ChildItem $LocalPath -recurse -include *.lnk
-
-# for each link
-foreach ($link in $links) {
-    $LShell = New-Object -comObject WScript.Shell
-    $linkFile = $link.FullName
-    $target = $WshShell.CreateShortcut($linkFile).targetpath
-    if(![System.IO.File]::Exists($target)) # if target does not exist, remove the link
-    {
-        Remove-Item -Path $linkFile -Force  
+if (![string]::IsNullOrEmpty($links))
+{
+    # for each link
+    foreach ($link in $links) {
+        $LShell = New-Object -comObject WScript.Shell
+        $linkFile = $link.FullName
+        $target = $WshShell.CreateShortcut($linkFile).targetpath
+        if(![System.IO.File]::Exists($target)) # if target does not exist, remove the link
+        {
+            Remove-Item -Path $linkFile -Force  
+        }
     }
 }
-
 
 # Get all files on the remote path
 $files = Get-ChildItem $RemotePath -recurse # there could be filters
