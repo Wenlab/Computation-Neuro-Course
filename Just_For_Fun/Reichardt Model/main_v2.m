6317043823b63aca45a5a9155a7e4e2005b2cc3e
Physics-Lee <hyperdunk2019@mail.ustc.edu.cn>
2023-10-06 22:58:08 +0800

    add disappearance method

--- Just_For_Fun/Reichardt Model/main_v2.m
+++ Just_For_Fun/Reichardt Model/main_v2.m
@@ -1,22 +1,21 @@
 %% prepare
 
 % clear
-clc;clear;close all;
+clc; clear; close all;
 
 % interface
 prompt = "Change color? 1 for yes and 0 for no.\n";
 is_changed = input(prompt);
-if is_changed ~=0 && is_changed ~= 1
+if is_changed ~= 0 && is_changed ~= 1
     error("Why so rebellious?")
 end
 
 % parameters
 width = 2000; % width of the window
 height = 300; % height of the window
-speed = 100 * 0.5; % unit: pixel per frame
+speed = 0; % unit: pixel per frame
 size_of_point = 10;
 frame_count = 0;
-n_change = 1;
 
 % set figure to grey
 figure('Color', [0.5 0.5 0.5], 'Position', [100, 100, width, height]);
@@ -30,32 +29,11 @@ set(gca, 'Color', [0.5 0.5 0.5]);
 method = "random_x_y";
 
 switch method
-    case "fix_x_y"
-
-        % init positions of points uniformly
-        n_rows = 20;
-        n_cols = 50;
-
-        dx = width / (n_cols - 1);
-        dy = height / (n_rows - 1);
-
-        [x, y] = meshgrid(0:dx:width, 0:dy:height);
-
-        x = x(:)';
-        y = y(:)';
-
-        % colors
-        n_points = n_rows * n_cols;
-        colors = [ones(1, n_points/2) zeros(1, n_points/2)];
-        colors = colors(randperm(n_points));
-
     case "random_x_y"
-
         n_points = 1000;
         x = rand(1, n_points) * width;
         y = rand(1, n_points) * height;
         colors = [ones(1, n_points/2) zeros(1, n_points/2)];
-
 end
 
 % draw init positions
@@ -69,8 +47,13 @@ while ishandle(gcf)
     % update
     frame_count = frame_count + 1;
 
-    % clear
+    % clear for moving
     cla;
+
+    % Change color or not, based on user choice
+    if is_changed
+        colors = 1 - colors;
+    end
 
     % update
     x = x + speed;
@@ -78,17 +61,21 @@ while ishandle(gcf)
     % periodic boundary condition
     x(x > width) = x(x > width) - width;
 
-    % change color
-    if is_changed && mod(frame_count, n_change) == 0
-        colors = 1 - colors;
-    end
-
     % plot
     plot(x(colors == 1), y(colors == 1), 'w.', 'MarkerSize', size_of_point);
     plot(x(colors == 0), y(colors == 0), 'k.', 'MarkerSize', size_of_point);
     drawnow;
 
     % pause
-    pause(0.01);
+    % pause(0.5); % Adjust the pause time if needed
 
+    % clear for disappearance
+    cla;
+
+    % Reinitialize random positions for reappearance
+    x = rand(1, n_points) * width;
+    y = rand(1, n_points) * height;
+
+    % pause for disappearance effect
+    % pause(0.5); % Adjust the pause time if needed
 end
